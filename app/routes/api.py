4805032cc536113efa51c75b551c7d01e47e7108
nfinnegan <nfinneg2@gmail.com>
2022-01-24 15:59:57 -0600

    added session to signup rte

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,5 +1,5 @@
 import sys
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, session
 from app.models import User
 from app.db import get_db
 
@@ -23,7 +23,13 @@ def signup():
     db.commit()
   except:
     print(sys.exc_info()[0])
+    # insert failed, so rollback and send error to front end
+    db.rollback()
     # insert failed, so send error to front end
     return jsonify(message = 'Signup failed'), 500
+  
+  session.clear()
+  session['user_id'] = newUser.id
+  session['loggedIn'] = True
 
   return jsonify(id = newUser.id)
